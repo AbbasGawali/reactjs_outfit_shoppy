b50fa38d8ad741bb914d5325f96e0aede41187ae
Abbas Gawali <abbasgawali111@gmail.com>
2024-01-24 10:40:31 +0530

    images issue2 resolved

--- src/components/form/SearchInput.jsx
+++ src/components/form/SearchInput.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useSearch } from '../../context/search'
+import { useSearch } from '../../context/Search.jsx'
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 const SearchInput = () => {
@@ -10,22 +10,16 @@ const SearchInput = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log("working 1 ........")
+
         try {
             const { data } = await axios.get(`${import.meta.env.VITE_API}/api/v1/product/search/${values.keyword}`);
             setValues({ ...values, results: data })
-            
-            console.log("working 1.2 ........")
-            console.log("keyword values " + values.keyword)
 
             navigate("/search");
         } catch (error) {
-            console.log("working 1.error ........")
             console.log(error);
         }
     }
-    // console.log("values is from search input");
-    // console.log(values);
     return (
         <div>
             <form className="d-flex" onSubmit={handleSubmit}>
