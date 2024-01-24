fedea89f75592feca6a26f012eccd7cbc4c79b83
Abbas Gawali <abbasgawali111@gmail.com>
2024-01-24 10:23:29 +0530

    link issue resolved

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/auth.jsx";
-import { SearchProvider } from "./context/search.jsx";
+import { SearchProvider } from "./context/Search.jsx";
 import { CartProvider } from "./context/Cart.jsx"
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
